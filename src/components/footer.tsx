97f768f167e5bb9646cf2a96167a77c82d063ce9
itsthankk <tkingsway3@gmail.com>
2023-12-04 08:17:04 +0100

    add all files

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <div className="w-full px-6 border-t border-t-gray-700 py-2 text-center">
+      An example of Account Abstraction: Creating a smart contract wallet!!!
+    </div>
+  );
+}
